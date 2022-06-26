d83f7dbb86e46afc64090685a7124c3c93b26f3b
Ignat <timofeev.ignat.i20@gmail.com>
2022-06-27 00:26:50 +0300

    br_10.4

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,16 @@
 var express = require('express');
 var router = express.Router();
+var Pokemon = require("../models/pokemon").Pokemon
 
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  Pokemon.find({},{_id:0,title:1,nick:1},function(err,menu){
+    res.render('index', {
+      title: 'Express',
+      menu: menu
+    });
+  })
 });
-
 module.exports = router;
+
--- routes/pokemons.js
+++ routes/pokemons.js
@@ -1,24 +1,33 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express')
+var router = express.Router()
 var Pokemon = require("../models/pokemon").Pokemon
-
+var async = require("async")
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-    res.send('Новый маршрутизатор, для маошрутов, начинающихся с pokemons');
+    res.send('Новый маршрутизатор, для маошрутов, начинающихся с pokemons')
 });
-
 /* Страница героев */
 router.get('/:nick', function(req, res, next) {
-    Pokemon.findOne({nick:req.params.nick}, function(err,pokemon){
-        if(err) return next(err)
-        if(!pokemon) return next(new Error("Нет такого покемона в этой книжке"))
-        res.render('pokemon', {
-            title: pokemon.title,
-            picture: pokemon.avatar,
-            desc: pokemon.desc
+    async.parallel([
+            function(callback){
+                Pokemon.findOne({nick:req.params.nick}, callback)
+            },
+            function(callback){
+                Pokemon.find({},{_id:0,title:1,nick:1},callback)
+            }
+        ],
+        function(err,result){
+            if(err) return next(err)
+            var pokemon = result[0]
+            var pokemons = result[1] || []
+            if(!pokemon) return next(new Error("Нет такого героя в этой книжке"))
+            res.render('pokemon', {
+                title: pokemon.title,
+                picture: pokemon.avatar,
+                desc: pokemon.desc,
+                menu: pokemons
+            });
         })
-    })
 })
 
-
-module.exports = router;
+module.exports = router
