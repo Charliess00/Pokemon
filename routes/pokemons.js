9a8d4d52c285a3fa37088274822aa57a47a5389b
Ignat <timofeev.ignat.i20@gmail.com>
2022-06-26 23:23:30 +0300

    br_10.3

--- routes/pokemons.js
+++ routes/pokemons.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var Pokemons = require("../models/pokemon").Pokemons
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -7,9 +8,17 @@ router.get('/', function(req, res, next) {
 });
 
 /* Страница героев */
-router.get("/:nick", function(req, res, next) {
-    res.send(req.params.nick);
-});
+router.get('/:nick', function(req, res, next) {
+    Pokemon.findOne({nick:req.params.nick}, function(err,pokemon){
+        if(err) return next(err)
+        if(!pokemon) return next(new Error("Нет такого покемона в этой книжке"))
+        res.render('pokemon', {
+            title: pokemon.title,
+            picture: pokemon.avatar,
+            desc: pokemon.desc
+        })
+    })
+})
 
 
 module.exports = router;
